9fc5474f3e05b7a86abc1e8780ea3808745738c3
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-05 11:08:45 -0400

    Server index message updated

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ const server = require('./api/server.js');
 
 const port = process.env.PORT || 3300;
 server.listen(port, () => {
-  console.log(`\n=== Server listening on port ${port}\n`);
+  console.log(`\n=== API running on http://localhost:${port} ===\n`);
 });
