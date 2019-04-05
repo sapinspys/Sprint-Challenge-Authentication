0c98074cb1134880e3168d8748db5ada7b56cb9d
Guillermo Arria-Devoe <garri009@fiu.edu>
2019-04-05 12:28:15 -0400

    Begun imported yesterday's react app structure

--- joke_client/src/App.js
+++ joke_client/src/App.js
@@ -1,6 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import { Route, Link, Redirect, withRouter } from 'react-router-dom'
+
 import logo from './logo.svg';
 import './App.css';
+
+import LoginForm from './components/LoginForm.js'
+import RegistrationForm from './components/RegistrationForm.js'
+import Home from './components/Home.js'
+
 
 class App extends Component {
   render() {
@@ -8,21 +15,41 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Link className='App-link' to="/">Home</Link>
+          &nbsp;&#124;&nbsp;
+          <Link className='App-link' to="/login">Login</Link>
+          &nbsp;&#124;&nbsp;
+          <Link className='App-link' to="/Register">Register</Link>
+          <button onClick={this.logout}>Logout</button>
         </header>
+        
+        <>
+          <PrivateRoute exact path='/' component={Home} />
+          <Route path='/login' component={LoginForm} />
+          <Route path='/register' component={RegistrationForm} />
+        </>
       </div>
     );
   }
+
+  logout = () => {
+    localStorage.removeItem('token');
+    alert('Successfully logged out!')
+    this.props.history.push('/login');
+  }
 }
 
-export default App;
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem('token') ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+)
+
+export default withRouter(App);
